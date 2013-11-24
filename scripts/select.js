2e3ef7267294a87ede48eeeecb84c64dc327dc43
Serhio Magpie <serdidg@gmail.com>
2013-11-24 22:12:44 +0200

    Select Reformat.

--- scripts/select.js
+++ scripts/select.js
@@ -1,72 +1,70 @@
 Com.Elements['Selects'] = {};
 
 Com['GetSelect'] = function(id){
-	return Com.Elements.Selects[id] || null;
+    return Com.Elements.Selects[id] || null;
 };
 
 Com['Select'] = function(o){
-	var that = this,
-		config = cm.merge({
+    var that = this, config = cm.merge({
             'container' : false,
-			'select' : cm.Node('select'),
+            'select' : cm.Node('select'),
             'multiple' : false,
             'showTitleTag' : true,
             'title' : false,
-			'menuMargin' : 3,
+            'menuMargin' : 3,
             'options' : [],
-			'selected' : 0,
-			'events' : {}
-		}, o),
+            'selected' : 0,
+            'events' : {}
+        }, o),
         dataAttributes = ['title', 'showTitleTag', 'multiple'],
-		API = {
-			'onSelect' : [],
-			'onChange' : [],
-			'onFocus' : [],
-			'onBlur' : []
-		},
-		nodes = {
+        API = {
+            'onSelect' : [],
+            'onChange' : [],
+            'onFocus' : [],
+            'onBlur' : []
+        },
+        nodes = {
             'menu' : {}
         },
         components = {},
-		options = {},
-		optionsList = [],
-		optionsLength,
-		
-		oldActive,
-		active;
-	
-	var init = function(){
-		// Convert events to API Events
-		convertEvents(config['events']);
+        options = {},
+        optionsList = [],
+        optionsLength,
+
+        oldActive, active;
+
+    var init = function(){
+        // Convert events to API Events
+        convertEvents(config['events']);
         // Merge data-attributes with config. Data-attributes have higher priority.
         processDataAttributes();
-		// Render
-		render();
-		setMiscEvents();
+        // Render
+        render();
+        setMiscEvents();
         // Set selected option
         if(config['multiple']){
             active = [];
-			if(config['selected'] && cm.isArray(config['selected'])){
+            if(config['selected'] && cm.isArray(config['selected'])){
                 cm.forEach(config['selected'], function(item){
                     if(options[item]){
                         set(options[item], true);
                     }
                 });
-			}else{
-				cm.forEach(config['select'].options, function(item){
-					item.selected && set(options[item.value]);
-				});
-			}
+            }else{
+                cm.forEach(config['select'].options, function(item){
+                    item.selected && set(options[item.value]);
+                });
+            }
         }else{
-			if(config['selected'] && options[config['selected']]){
-				set(options[config['selected']]);
-			}else if(config['select'].value){
-				set(options[config['select'].value]);
-			}else if(optionsLength){
-				set(optionsList[0]);
-			}
+            if(config['selected'] && options[config['selected']]){
+                set(options[config['selected']]);
+            }else if(config['select'].value){
+                set(options[config['select'].value]);
+            }else if(optionsLength){
+                set(optionsList[0]);
+            }
         }
-	};
+    };
 
     var processDataAttributes = function(){
         var value;
@@ -77,7 +75,7 @@ Com['Select'] = function(o){
             }else if(item == 'multiple'){
                 value = config['select'].multiple;
             }else if(/^false|true$/.test(value)){
-                value = value? (value == 'true') : config[item];
+                value = value ? (value == 'true') : config[item];
             }else{
                 value = value || config[item];
             }
@@ -86,119 +84,101 @@ Com['Select'] = function(o){
     };
 
     var render = function(){
-		var tabindex;
-		/* *** RENDER STRUCTURE *** */
-		if(config['multiple']){
+        var tabindex;
+        /* *** RENDER STRUCTURE *** */
+        if(config['multiple']){
             renderMultiple();
         }else{
             renderSingle();
         }
-		/* *** ATTRIBUTES *** */
-		// Set select width
-		if(config['select'].offsetWidth && config['select'].offsetWidth != config['select'].parentNode.offsetWidth){
+        /* *** ATTRIBUTES *** */
+        // Set select width
+        if(config['select'].offsetWidth && config['select'].offsetWidth != config['select'].parentNode.offsetWidth){
             nodes['container'].style.width = config['select'].offsetWidth + 'px';
-		}
-		// Add class name
-		if(config['select'].className){
-			cm.addClass(nodes['container'], config['select'].className);
-		}
+        }
+        // Add class name
+        if(config['select'].className){
+            cm.addClass(nodes['container'], config['select'].className);
+        }
         // Title
         if(config['showTitleTag'] && config['title']){
             nodes['container'].title = config['title'];
         }
-		// Tabindex
-		if(tabindex = config['select'].getAttribute('tabindex')){
-			nodes['container'].setAttribute('tabindex', tabindex);
-		}
-		// ID
-		if(config['select'].id){
-			nodes['container'].id = config['select'].id;
-		}
-		// Data
-		Array.prototype.forEach.call(config['select'].attributes, function(item){
-			if(/^data-/.test(item.name)){
-				nodes['container'].setAttribute(item.name, item.value);
-			}
-		});
+        // Tabindex
+        if(tabindex = config['select'].getAttribute('tabindex')){
+            nodes['container'].setAttribute('tabindex', tabindex);
+        }
+        // ID
+        if(config['select'].id){
+            nodes['container'].id = config['select'].id;
+        }
+        // Data
+        Array.prototype.forEach.call(config['select'].attributes, function(item){
+            if(/^data-/.test(item.name)){
+                nodes['container'].setAttribute(item.name, item.value);
+            }
+        });
         // Set hidden input attributes
         if(config['select'].getAttribute('name')){
             nodes['hidden'].setAttribute('name', config['select'].getAttribute('name'));
         }
-		/* *** RENDER OPTIONS *** */
+        /* *** RENDER OPTIONS *** */
         cm.forEach(config['select'].options, function(item){
             renderOption(item.value, item.innerHTML);
         });
         cm.forEach(config['options'], function(item){
             renderOption(item.value, item.text);
         });
-		/* *** INSERT INTO DOM *** */
+        /* *** INSERT INTO DOM *** */
         if(config['container']){
             config['container'].appendChild(nodes['container']);
         }else if(config['select'].parentNode){
-		    cm.insertBefore(nodes['container'], config['select']);
+            cm.insertBefore(nodes['container'], config['select']);
         }
-		cm.remove(config['select']);
-	};
+        cm.remove(config['select']);
+    };
 
     var renderSingle = function(){
-        nodes['container'] = cm.Node('div', {'class':'cm-select'},
-            nodes['hidden'] = cm.Node('select', {'data-select' : 'false', 'class' : 'display-none'}),
-            nodes['input'] = cm.Node('div', {'class':'cm-select-input clear'},
-                cm.Node('div', {'class':'cm-select-inner'},
-                    nodes['arrow'] = cm.Node('div', {'class':'cm-select-arrow'}),
-                    nodes['text'] = cm.Node('div', {'class':'cm-select-text'})
-                )
-            ),
-            nodes['scroll'] = cm.Node('div', {'class':'cm-select-listing'},
-                nodes['items'] = cm.Node('ul')
-            )
-        );
+        nodes['container'] = cm.Node('div', {'class' : 'cm-select'}, nodes['hidden'] = cm.Node('select', {'data-select' : 'false', 'class' : 'display-none'}), nodes['input'] = cm.Node('div', {'class' : 'cm-select-input clear'}, cm.Node('div', {'class' : 'cm-select-inner'}, nodes['arrow'] = cm.Node('div', {'class' : 'cm-select-arrow'}), nodes['text'] = cm.Node('div', {'class' : 'cm-select-text'}))), nodes['scroll'] = cm.Node('div', {'class' : 'cm-select-listing'}, nodes['items'] = cm.Node('ul')));
     };
 
     var renderMultiple = function(){
-        nodes['container'] = cm.Node('div', {'class' : 'cm-multiselect'},
-            nodes['hidden'] = cm.Node('select', {'data-select' : 'false', 'class' : 'display-none', 'multiple' : true}),
-            nodes['inner'] = cm.Node('div', {'class' : 'inner'},
-                nodes['scroll'] = cm.Node('div', {'class' : 'cm-select-listing'},
-                    nodes['items'] = cm.Node('ul')
-                )
-            )
-        );
+        nodes['container'] = cm.Node('div', {'class' : 'cm-multiselect'}, nodes['hidden'] = cm.Node('select', {'data-select' : 'false', 'class' : 'display-none', 'multiple' : true}), nodes['inner'] = cm.Node('div', {'class' : 'inner'}, nodes['scroll'] = cm.Node('div', {'class' : 'cm-select-listing'}, nodes['items'] = cm.Node('ul'))));
     };
-	
-	var renderOption = function(value, text){
-		// Check for exists
-		if(options[value]){
-			removeOption(options[value]);
-		}
-		// Config
-		var item = {
+
+    var renderOption = function(value, text){
+        // Check for exists
+        if(options[value]){
+            removeOption(options[value]);
+        }
+        // Config
+        var item = {
             'node' : nodes['items'].appendChild(cm.Node('li', {'innerHTML' : text})),
             'option' : nodes['hidden'].appendChild(cm.Node('option', {'value' : value, 'innerHTML' : text})),
             'selected' : false,
-			'value' : value,
-			'text' : text
-		};
-		// Label onlick event
-		item['node'].onclick = function(){
-			set(item, true);
+            'value' : value,
+            'text' : text
+        };
+        // Label onlick event
+        item['node'].onclick = function(){
+            set(item, true);
             !config['multiple'] && components['menu'].hide(false);
-		};
-		// Push
-		optionsList.push(options[value] = item);
-		optionsLength = optionsList.length;
-	};
-	
-	var removeOption = function(option){
+        };
+        // Push
+        optionsList.push(options[value] = item);
+        optionsLength = optionsList.length;
+    };
+
+    var removeOption = function(option){
         var value = option['value'] || option['text'];
-		// Remove option from list and array
-		cm.remove(option['node']);
+        // Remove option from list and array
+        cm.remove(option['node']);
         cm.remove(option['option']);
-		optionsList = optionsList.filter(function(item){
-			return option != item;
-		});
-		optionsLength = optionsList.length;
-		delete options[option['value']];
+        optionsList = optionsList.filter(function(item){
+            return option != item;
+        });
+        optionsLength = optionsList.length;
+        delete options[option['value']];
         // Set new active option, if current active is nominated for remove
         if(config['multiple']){
             active = active.filter(function(item){
@@ -214,15 +194,14 @@ Com['Select'] = function(o){
                 }
             }
         }
-	};
-	
-	var setMiscEvents = function(){
-		// Switch items on arrows press
-		cm.addEvent(nodes['container'], 'keydown', function(e){
-			e = cm.getEvent(e);
+    };
+
+    var setMiscEvents = function(){
+        // Switch items on arrows press
+        cm.addEvent(nodes['container'], 'keydown', function(e){
+            e = cm.getEvent(e);
             if(optionsLength){
-                var item = options[active],
-                    index = optionsList.indexOf(item);
+                var item = options[active], index = optionsList.indexOf(item);
                 if(e.keyCode == 38){
                     if(index - 1 >= 0){
                         set(optionsList[index - 1], true);
@@ -237,13 +216,13 @@ Com['Select'] = function(o){
                     }
                 }
             }
-		});
-		cm.addEvent(nodes['container'], 'focus', function(){
-			cm.addEvent(document.body, 'keydown', blockDocumentArrows)
-		});
-		cm.addEvent(nodes['container'], 'blur', function(){
-			cm.removeEvent(document.body, 'keydown', blockDocumentArrows)
-		});
+        });
+        cm.addEvent(nodes['container'], 'focus', function(){
+            cm.addEvent(document.body, 'keydown', blockDocumentArrows)
+        });
+        cm.addEvent(nodes['container'], 'blur', function(){
+            cm.removeEvent(document.body, 'keydown', blockDocumentArrows)
+        });
 
         if(!config['multiple']){
             // Render tooltip
@@ -262,7 +241,7 @@ Com['Select'] = function(o){
             });
             nodes['menu'] = components['menu'].getNodes();
         }
-	};
+    };
 
     var show = function(){
         if(!optionsLength){
@@ -287,8 +266,8 @@ Com['Select'] = function(o){
         /* *** EXECUTE API EVENTS *** */
         executeEvent('onBlur');
     };
-	
-	var set = function(option, execute){
+
+    var set = function(option, execute){
         if(option){
             if(config['multiple']){
                 setMultiple(option);
@@ -301,7 +280,7 @@ Com['Select'] = function(o){
             executeEvent('onSelect');
             executeEvent('onChange');
         }
-	};
+    };
 
     var setMultiple = function(option){
         var value = option['value'] || option['text'];
@@ -326,57 +305,55 @@ Com['Select'] = function(o){
         optionsList.forEach(function(item){
             cm.removeClass(item['node'], 'active');
         });
-        cm.clearNode(nodes['text']).appendChild(
-            cm.Node('span', {'innerHTML': option['text']})
-        );
+        cm.clearNode(nodes['text']).appendChild(cm.Node('span', {'innerHTML' : option['text']}));
         option['option'].selected = true;
         cm.addClass(option['node'], 'active');
     };
-	
-	var executeEvent = function(event){
-		var handler = function(){
-			cm.forEach(API[event], function(item){
-				item(that, active);
-			});
-		};
-		
-		switch(event){
-			case 'onChange':
+
+    var executeEvent = function(event){
+        var handler = function(){
+            cm.forEach(API[event], function(item){
+                item(that, active);
+            });
+        };
+
+        switch(event){
+            case 'onChange':
                 if(config['multiple']){
                     handler();
                 }else{
                     active != oldActive && handler();
                 }
-			break;
-			
-			default:
-				handler();
-			break;
-		}
-	};
-	
-	var convertEvents = function(o){
-		cm.forEach(o, function(item, key){
-			if(API[key] && typeof item == 'function'){
-				API[key].push(item);
-			}
-		});
-	};
-	
-	var blockDocumentArrows = function(e){
-		e = cm.getEvent(e);
-		if(e.keyCode == 38 || e.keyCode == 40){
-            cm.preventDefault(e);
-		}
-	};
-	
-	/* *** MAIN *** */
+                break;
 
-	that.get = function(){
-		return active;
-	};
-	
-	that.set = function(value){
+            default:
+                handler();
+                break;
+        }
+    };
+
+    var convertEvents = function(o){
+        cm.forEach(o, function(item, key){
+            if(API[key] && typeof item == 'function'){
+                API[key].push(item);
+            }
+        });
+    };
+
+    var blockDocumentArrows = function(e){
+        e = cm.getEvent(e);
+        if(e.keyCode == 38 || e.keyCode == 40){
+            cm.preventDefault(e);
+        }
+    };
+
+    /* *** MAIN *** */
+
+    that.get = function(){
+        return active;
+    };
+
+    that.set = function(value){
         // Select option and execute events
         if(typeof value != 'undefined'){
             if(cm.isArray(value)){
@@ -392,36 +369,36 @@ Com['Select'] = function(o){
                 set(options[value], true);
             }
         }
-		return that;
-	};
-	
-	that.addEvent = function(event, handler){
-		if(API[event] && typeof handler == 'function'){
-			API[event].push(handler);
-		}
-		return that;
-	};
-	
-	that.removeEvent = function(event, handler){
-		if(API[event] && typeof handler == 'function'){
-			API[event] = API[event].filter(function(item){
-				return item != handler;
-			});
-		}
-		return that;
-	};
-	
-	that.addOption = function(value, text){
-		renderOption(value, text);
-		return that;
-	};
-	
-	that.removeOption = function(value){
-		if(value && options[value]){
-			removeOption(options[value]);
-		}
-		return that;
-	};
+        return that;
+    };
+
+    that.addEvent = function(event, handler){
+        if(API[event] && typeof handler == 'function'){
+            API[event].push(handler);
+        }
+        return that;
+    };
+
+    that.removeEvent = function(event, handler){
+        if(API[event] && typeof handler == 'function'){
+            API[event] = API[event].filter(function(item){
+                return item != handler;
+            });
+        }
+        return that;
+    };
+
+    that.addOption = function(value, text){
+        renderOption(value, text);
+        return that;
+    };
+
+    that.removeOption = function(value){
+        if(value && options[value]){
+            removeOption(options[value]);
+        }
+        return that;
+    };
 
     that.addOptions = function(arr){
         cm.forEach(arr, function(item){
@@ -445,27 +422,26 @@ Com['Select'] = function(o){
         o && convertEvents(o);
         return that;
     };
-	
-	init();
+
+    init();
 };
 
 Com['SelectCollector'] = function(node){
-	var selects,
-        id,
-		select;
-		
-	var init = function(node){
-		if(!node){
-			render(document.body);
-		}else if(node.constructor == Array){
+    var selects, id, select;
+
+    var init = function(node){
+        if(!node){
+            render(document.body);
+        }else if(node.constructor == Array){
             cm.forEach(node, render);
-		}else{
-			render(node);
-		}
-	};
+        }else{
+            render(node);
+        }
+    };
 
     var render = function(node){
-        selects = cm.clone((node.nodeType == 1 && node.tagName.toLowerCase() == 'select') ? [node] : node.getElementsByTagName('select'));
+        selects = cm.clone((node.nodeType == 1 && node.tagName.toLowerCase() == 'select') ?
+            [node] : node.getElementsByTagName('select'));
         // Render datepickers
         cm.forEach(selects, function(item){
             if(!/^norender|false$/.test(item.getAttribute('data-select'))){
@@ -476,6 +452,6 @@ Com['SelectCollector'] = function(node){
             }
         });
     };
-	
-	init(node);
+
+    init(node);
 };
