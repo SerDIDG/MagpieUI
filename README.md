0f2112461480ce065a1b9c5c79e0c14f1e85c75b
SerDIDG <serdidg@gmail.com>
2014-06-14 01:27:26 +0300

    * MD. Fix.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ FioraUI
 Simple and lightweight framework that allows you to build UI quickly as possible.
 
 Attention! Newer versions of FioraUI library may not be compatible with the previous ones!
+
+Developed with PhpStorm.
---
+++ js/components/old/CollapsibleLayout.js
@@ -0,0 +1,114 @@
+Com['CollapsibleLayout'] = function(o){
+    var that = this,
+        config = cm.merge({
+            'node' : cm.Node('div'),
+            'events' : {},
+            'nodes' : {}
+        }, o),
+        API = {
+            'onRender' : [],
+            'onCollapse' : [],
+            'onExpand' : [],
+            'onChange' : []
+        },
+        nodes = {
+            'leftButton' : cm.Node('div'),
+            'rightButton': cm.Node('div')
+        };
+
+    /* *** CLASS FUNCTIONS *** */
+
+    var init = function(){
+        convertEvents(config['events']);
+        getNodes(config['node'], 'ComCollapsibleLayout');
+        render();
+    };
+
+    var render = function(){
+        // Left Sidebar
+        cm.addEvent(nodes['leftButton'], 'click', function(){
+            if(cm.isClass(config['node'], 'is-sidebar-left-collapsed')){
+                cm.removeClass(config['node'], 'is-sidebar-left-collapsed', true);
+                // API onExpand event
+                executeEvent('onExpand', {'sidebar' : 'left'});
+            }else{
+                cm.addClass(config['node'], 'is-sidebar-left-collapsed', true);
+                // API onCollapse event
+                executeEvent('onCollapse', {'sidebar' : 'left'});
+            }
+            // API onChange event
+            executeEvent('onChange', {
+                'sidebar' : 'left',
+                'isCollapsed' : cm.isClass(config['node'], 'is-sidebar-left-collapsed')
+            });
+        });
+        // Right sidebar
+        cm.addEvent(nodes['rightButton'], 'click', function(){
+            if(cm.isClass(config['node'], 'is-sidebar-right-collapsed')){
+                cm.removeClass(config['node'], 'is-sidebar-right-collapsed', true);
+                // API onExpand event
+                executeEvent('onExpand', {'sidebar' : 'right'});
+            }else{
+                cm.addClass(config['node'], 'is-sidebar-right-collapsed', true);
+                // API onCollapse event
+                executeEvent('onCollapse', {'sidebar' : 'right'});
+            }
+            // API onChange event
+            executeEvent('onChange', {
+                'sidebar' : 'right',
+                'isCollapsed' : cm.isClass(config['node'], 'is-sidebar-right-collapsed')
+            });
+        });
+        // API onRender event
+        executeEvent('onRender');
+    };
+
+    /* *** MISC FUNCTIONS *** */
+
+    var convertEvents = function(o){
+        cm.forEach(o, function(item, key){
+            if(API[key] && typeof item == 'function'){
+                API[key].push(item);
+            }
+        });
+    };
+
+    var getNodes = function(container, marker){
+        if(container){
+            var sourceNodes = {};
+            if(marker){
+                sourceNodes = cm.getNodes(container)[marker] || {};
+            }else{
+                sourceNodes = cm.getNodes(container);
+            }
+            nodes = cm.merge(nodes, sourceNodes);
+        }
+        nodes = cm.merge(nodes, config['nodes']);
+    };
+
+    var executeEvent = function(event, params){
+        API[event].forEach(function(item){
+            item(that, params || {});
+        });
+    };
+
+    /* ******* MAIN ******* */
+
+    that.addEvent = function(event, handler){
+        if(API[event] && typeof handler == 'function'){
+            API[event].push(handler);
+        }
+        return that;
+    };
+
+    that.removeEvent = function(event, handler){
+        if(API[event] && typeof handler == 'function'){
+            API[event] = API[event].filter(function(item){
+                return item != handler;
+            });
+        }
+        return that;
+    };
+
+    init();
+};
