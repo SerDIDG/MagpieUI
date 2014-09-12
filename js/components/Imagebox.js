1c39e06978d8b5b92781bc8260243bef396b7fb5
Serhio Magpie <serdidg@gmail.com>
2014-09-12 20:07:30 +0300

    * JS. Renamed file.

--- js/components/Imagebox.js
+++ js/components/Imagebox.js
@@ -10,11 +10,14 @@ cm.define('Com.ImageBox', {
     'params' : {
         'node' : cm.Node('div'),
         'animated' : false,
-        'effect' : 'none'
+        'effect' : 'none',
+        'scrollNode' : 'document.window'
     }
 },
 function(params){
-    var that = this;
+    var that = this,
+        dimensions = {},
+        pageDimensions = {};
 
     that.procced = false;
 
@@ -27,9 +30,54 @@ function(params){
 
     var render = function(){
         if(that.params['animated']){
-
+            cm.addClass(that.params['node'], ['pre', that.params['effect']].join('-'));
+            cm.addEvent(that.params['scrollNode'], 'scroll', process);
+            process();
         }
         that.triggerEvent('onRender');
+    };
+
+    var process = function(){
+        if(!that.procced){
+            getDimensions();
+            getPageDimensions();
+            // Rules for different block sizes.
+            if(dimensions['height'] < pageDimensions['winHeight']){
+                // Rules for block, which size is smaller than page's.
+                if(
+                    dimensions['y1'] >= 0 &&
+                    dimensions['y2'] <= pageDimensions['winHeight']
+                ){
+                    doProcess();
+                }
+            }else{
+                // Rules for block, which size is larger than page's.
+                if(
+                    (dimensions['y1'] < 0 && dimensions['y2'] >= pageDimensions['winHeight'] / 2) ||
+                    (dimensions['y2'] > pageDimensions['winHeight'] && dimensions['y1'] <= pageDimensions['winHeight'] / 2)
+                ){
+                    doProcess();
+                }
+            }
+        }
+    };
+
+    var doProcess = function(){
+        that.procced = true;
+        cm.addClass(that.params['node'], ['animated', that.params['effect']].join(' '));
+    };
+    
+    var getDimensions = function(){
+        dimensions['width'] = that.params['node'].offsetWidth;
+        dimensions['height'] = that.params['node'].offsetHeight;
+        dimensions['x1'] = cm.getRealX(that.params['node']);
+        dimensions['y1'] = cm.getRealY(that.params['node']);
+        dimensions['x2'] = dimensions['x1'] + dimensions['width'];
+        dimensions['y2'] = dimensions['y1'] + dimensions['height'];
+    };
+
+    var getPageDimensions = function(){
+        pageDimensions = cm.getPageSize();
     };
 
     /* ******* MAIN ******* */
