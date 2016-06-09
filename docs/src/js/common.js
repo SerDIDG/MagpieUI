f78348a8613c958738f83467b9341e76a8534e31
Serhio Magpie <serdidg@gmail.com>
2016-06-09 19:40:16 +0300

    DOCS

--- docs/src/js/common.js
+++ docs/src/js/common.js
@@ -1,1 +1,5 @@
-cm._baseUrl = [cm._baseUrl, 'docs/build'].join('/');
+if(cm._baseUrl.indexOf('serdidg.github.io/MagpieUI/') > -1){
+    cm._baseUrl = [cm._baseUrl, '/MagpieUI/docs/build'].join('/');
+}else{
+    cm._baseUrl = [cm._baseUrl, 'docs/build'].join('/');
+}
