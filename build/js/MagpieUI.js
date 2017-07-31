6de887fb0f33fb58462d3e43d27f187401aec6a3
Serhio Magpie <serdidg@gmail.com>
2017-07-31 19:50:43 +0300

    *

--- build/js/MagpieUI.js
+++ build/js/MagpieUI.js
@@ -1,4 +1,4 @@
-/*! ************ MagpieUI v3.29.2 (2017-07-31 19:47) ************ */
+/*! ************ MagpieUI v3.29.2 (2017-07-31 19:49) ************ */
 // TinyColor v1.3.0
 // https://github.com/bgrins/TinyColor
 // Brian Grinstead, MIT License
